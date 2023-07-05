c3b5fa30e7ef6c467771475e077a4d30578a278e
rgcbrajesh <brajeshrajpu612@gmail.com>
2023-07-05 11:16:32 +0530

    initial commit

---
+++ js/front.js
@@ -0,0 +1,18 @@
+const counters=document.querySelectorAll('.counter');
+// console.log("hello");
+counters.forEach((counter)=>{
+counter.innerHTML=0;
+const updatecounter=()=>{
+const targetcount=+counter.getAttribute('data-target');
+const startingCount=Number(counter.innerHTML);
+const incr=targetcount/100;
+if(startingCount<targetcount){
+    counter.innerHTML=`${Math.round(startingCount+incr)}`;
+    setTimeout(updatecounter,10)
+}
+else{
+    counter.innerHTML=targetcount;
+}
+}
+updatecounter();
+})
